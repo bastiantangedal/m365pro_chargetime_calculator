769b0ea83c46accbc29706a4cf130af8a1d221e6
Bastian Pedersen <bastian.Tangedal@gmail.com>
2020-07-06 17:57:10 +0200

    Minor refactoring and styling

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import sys
 from datetime import timedelta
+import time
 
 BATTERY_CAPACITY = 12800 # in mAh
 CHARGE_CURRENT = 1700 # in mAh
 CHARGE_EFFICIENCY = 1.1 # 90% 
+
+def printUsage(text, color):
+  print(f"{color}{text}")
 
 def main():
   try:
@@ -11,17 +15,22 @@ def main():
     currentPercentage = int(sys.argv[1])
     desiredPercentage = int(sys.argv[2])
 
-    currentPercentageInMah = (BATTERY_CAPACITY / 100) * currentPercentage
+    printUsage("Tool created by: Bastian Pedersen.", "\u001b[33;1m")
+    printUsage("NB: This tool is not 100% accurate. Factors such as temperature affect charging speed.", "\u001b[31;1m")
+    time.sleep(1.5)
+
+    currentPercentageInMah = int((BATTERY_CAPACITY / 100) * currentPercentage)
     remainingMah = BATTERY_CAPACITY - currentPercentageInMah
 
     # Calculate charge time
-    chargeTimeInMinutes = round((remainingMah / CHARGE_CURRENT) * CHARGE_EFFICIENCY, 1) * 60
+    chargeTimeInMinutes = int(round((remainingMah / CHARGE_CURRENT) * CHARGE_EFFICIENCY, 1) * 60)
     chargeTimeInTimeFormat = str(timedelta(minutes=chargeTimeInMinutes))[:-3]
     chargeTimeHours = chargeTimeInTimeFormat.split(":")
 
     # Print out result
-    print(f"Battery currently has: {currentPercentage}% \nYou want: {desiredPercentage}%")
-    print(f"""\nThis will approximately take: {chargeTimeInTimeFormat.split(':')[0]} hour(s) and {chargeTimeInTimeFormat.split(':')[1]} minutes, with a charge efficiency of {(int(0.9*100))}%.""")  
+    printUsage(f"\nBattery currently has: {currentPercentage}% \nYou want: {desiredPercentage}%", "\u001b[37m")
+    printUsage(f"""\nThis will approximately take: {chargeTimeHours[0]} hour(s) and {chargeTimeHours[1]} minutes, with a charge efficiency of {(int(0.9*100))}%.""", "\u001b[37m")
+    
   except Exception:
     raise ValueError("usage: (currentPercentage) (desiredPercentage) // example: 30 70") from None
 
